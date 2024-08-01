62ffcf7a74e7a2684612fffaadf475c40365f288
lesuuda <lesuudaljamani@gmail.com>
2024-08-01 11:51:11 +0300

    fixed typed annotation for inmput parameters

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,9 +3,13 @@
 
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """filters a logged file"""
-    pattern = r'(' + '|'.join(re.escape(field) for field in fields) + r')=[^' + re.escape(separator) + r']*'
-    return re.sub(pattern, lambda m: m.group().split('=')[0] + '=' + redaction, message)
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
