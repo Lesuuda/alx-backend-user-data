62f1e45b54a0d9ffe25c9fe980bf305f3727ac66
lesuuda <lesuudaljamani@gmail.com>
2024-08-01 11:38:30 +0300

    core: added filtered_logger.py

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env pyhthon3
+"""filters a logged file"""
+
+
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """filters a logged file"""
+    pattern = (r'(' + '|'.join(re.escape(field) for field in fields) +
+               r')=[^' + re.escape(separator) + r']*')
+    return (re.sub(pattern, lambda m: m.group().split('=')[0] +
+                   '=' + redaction, message))
