2dabe2f5de0a2024f26e413dbc6847944555b130
lesuuda <lesuudaljamani@gmail.com>
2024-08-01 11:44:34 +0300

    made function less than four lines

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pyhthon3
 """filters a logged file"""
 
 
@@ -7,7 +7,5 @@ import re
 
 def filter_datum(fields, redaction, message, separator) -> str:
     """filters a logged file"""
-    pattern = (r'(' + '|'.join(re.escape(field) for field in fields) +
-               r')=[^' + re.escape(separator) + r']*')
-    return (re.sub(pattern, lambda m: m.group().split('=')[0] +
-                   '=' + redaction, message))
+    pattern = r'(' + '|'.join(re.escape(field) for field in fields) + r')=[^' + re.escape(separator) + r']*'
+    return re.sub(pattern, lambda m: m.group().split('=')[0] + '=' + redaction, message)
