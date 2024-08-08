4320245333970a1c943006df85e2066af46a811b
lesuuda <lesuudaljamani@gmail.com>
2024-08-08 11:06:10 +0300

    core: added implementation for forbidden using error code 403

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,11 @@ def unauthorized() -> None:
     Raises a 401 Unathorized error
     """
     abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> None:
+    """
+    GET /api/v1/forbidden
+    """
+    abort(403)
