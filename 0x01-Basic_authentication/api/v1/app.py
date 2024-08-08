2663fcce50b0b72f84e6b2e9b6c64694837b0f1d
lesuuda <lesuudaljamani@gmail.com>
2024-08-08 16:05:42 +0300

    fix: fixed code for forbiden functionality

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -34,7 +34,9 @@ def unauthorized(error) -> str:
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """error handler for forbidden"""
-    return jsonify({"error": "Forbidden"})
+    return jsonify({
+        "error": "Forbidden"
+        }), 403
 
 
 if __name__ == "__main__":
