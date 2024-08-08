13339732ba4f9c8f6a434551fa1e8f2fbff61f27
lesuuda <lesuudaljamani@gmail.com>
2024-08-08 22:10:23 +0300

    core: implemented Basic- Base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,13 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """class BasicAuth that inherits from Auht"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """perfoms base_64 ebcoding"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
