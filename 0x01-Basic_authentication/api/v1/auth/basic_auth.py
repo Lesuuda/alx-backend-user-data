d55b62bb1d49370cb05c8a37dfc308094fa6d902
lesuuda <lesuudaljamani@gmail.com>
2024-08-08 22:20:42 +0300

    core: added base64 decoding basic

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """implementation of basic_auth that inherits from auth"""
 
-
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -17,3 +17,17 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """implemnts basic base64 decode"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decode_bytes = base64.b64decode(base64_authorization_header)
+            return decode_bytes.decode('utf-8')
+        except Exception:
+            return None
