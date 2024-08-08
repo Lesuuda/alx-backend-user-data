f878e305c6feecd99e14c9c745c0ff8cc865bd90
lesuuda <lesuudaljamani@gmail.com>
2024-08-08 16:32:13 +0300

    fix: fixed code output for require_auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,7 @@ from typing import List, TypeVar
 
 class Auth:
     """class implementation of Auth"""
-    def require_auth(self, path: str, excluded_paths: List[str]) -> str:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         returns false path and excluded paths
         """
