8aae514028fe654bec184956f06924015cdf859a
lesuuda <lesuudaljamani@gmail.com>
2024-08-09 15:12:28 +0300

    core: added create session for session authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,8 +3,19 @@
 
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """class impmenetation for session auth"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a session id for user id"""
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        SessionAuth.user_id_by_session_id.update({session_id: user_id})
+        return session_id
