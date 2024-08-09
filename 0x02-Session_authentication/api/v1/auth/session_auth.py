576dd3c5f741b7bbd9c052e244c9ff95ed80ed5e
lesuuda <lesuudaljamani@gmail.com>
2024-08-09 20:55:11 +0300

    core:used user id to identify user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,7 +4,7 @@
 
 from api.v1.auth.auth import Auth
 import uuid
-
+from models.user import User
 
 class SessionAuth(Auth):
     """class impmenetation for session auth"""
@@ -27,3 +27,13 @@ class SessionAuth(Auth):
         if not isinstance(session_id, str):
             return None
         return SessionAuth.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """returns a user instance based on cookie value"""
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+        return User.get(user_id)
