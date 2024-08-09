7fad50266bf5470c63f067a122781aeda4e191f7
lesuuda <lesuudaljamani@gmail.com>
2024-08-09 14:29:39 +0300

    core: first trial etimoi

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""class implementation of Auth"""
+
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """class implementation of Auth"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        returns True if path not in list of strings to exclude
+        """
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path is None:
+            return True
+        normalized_path = path if path.endswith('/') else path + '/'
+        for excluded_path in excluded_paths:
+            if excluded_path == normalized_path:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """returns None"""
+        if request is None:
+            return None
+        key = 'Authorizaion'
+        if key not in request.headers:
+            return None
+        return request.headers['Authorization']
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """currently returns None"""
+        return None
